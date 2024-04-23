96faf38c2f90ecdb3ed15641ecd97e6b069e1c56
shayan <shayan.rasouli95@gmail.com>
2024-04-24 03:22:47 +0330

    Set (bootstrap & cards)

--- wwwroot/assets/js/script.js
+++ wwwroot/assets/js/script.js
@@ -2,18 +2,14 @@ let menu = document.querySelector("#menu-bar");
 let navbar = document.querySelector(".navbar");
 
 window.onscroll = () => {
-  searchBtn.classList.remove("fa-times");
-  searchBar.classList.remove("active");
   menu.classList.remove("fa-times");
   navbar.classList.remove("active");
-  loginForm.classList.remove("active");
 };
 
 menu.addEventListener("click", () => {
   menu.classList.toggle("fa-times");
   navbar.classList.toggle("active");
 });
-
 
 //Initialize Swiper
 var swiper = new Swiper(".mySwiper", {
