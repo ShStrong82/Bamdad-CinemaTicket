d6dc56488b93c8a0c23659985e2d9f3a6db92567
shayan <shayan.rasouli95@gmail.com>
2024-04-25 01:48:03 +0330

    set js data and generate dynamicly card

--- wwwroot/assets/js/script.js
+++ wwwroot/assets/js/script.js
@@ -28,3 +28,61 @@ var swiper = new Swiper(".mySwiper", {
     el: ".swiper-pagination",
   },
 });
+
+var moviesArray = [
+  {
+    title: "بی بدن",
+    description:
+      "بی بدن نخستین ساخته‌ی مزتضی علیزاده به‌عنوان کارگردان با بازی نوید پورفرج، نویسندگی کاظم دانشی و تهیه‌کنندگی سیدمصطفی احمدی است که در چهل و دومین جشنواره فجر در بخش نگاه نو حضور دارد.",
+    image: "./assets/img/bodiless_movie.jpg",
+    button: "#",
+  },
+  {
+    title: "تمساح خونی",
+    description:
+      "تمساح خونی نخستین ساخته‌ی جواد عزتی به‌عنوان کارگردان و به تهیه‌کنندگی کامران حجازی است که در چهل و دومین جشنواره فیلم فجر نیز حضور داشت.",
+    image: "./assets/img/TemsahKhooni_movie.jpeg",
+    button: "#",
+  },
+  {
+    title: "آسمان غرب",
+    description:
+      "فیلم آسمان غرب به کارگردانی محمد عسگری و تهیه‌کنندگی حبیب‌الله والی‌نژاد است که در چهل و دومین جشنواره فجر حضور دارد.",
+    image: "./assets/img/AsemaneGhrarb_movie.jpeg",
+    button: "#",
+  },
+  {
+    title: "سال گربه",
+    description:
+      "نمایش سال گربه به کارگردانی مصطفی تقی‌زاده و تهیه‌کنندگی سعید خانی است. بهرام افشاری، سارا بهرامی، هومن برق‌نورد، شبنم قربانی، نوشین تبریزی و رضا رشیدپور بازیگران این فیلم هستند.",
+    image: "./assets/img/SaleGorbe_movie.jpg",
+    button: "#",
+  },
+];
+
+function addCard() {
+  var cardContainer = document.getElementById("cardContainer");
+
+  for (var i = 0; i < moviesArray.length; i++) {
+    var card = moviesArray[i];
+
+    var cardHtml = `<div class="col">
+    <div class="card h-100">
+      <img
+        src="${card.image}"
+        class="card-img-top"
+        alt="${card.title}"
+      />
+      <div class="card-body">
+        <h5 class="card-title">${card.title}</h5>
+        <p class="card-text">
+        ${card.description}
+        </p>
+      </div>
+      <a class="btn btn-primary" href="${card.button}">خرید بلیط</a>
+    </div>
+  </div>`;
+
+    cardContainer.innerHTML += cardHtml;
+  }
+}
